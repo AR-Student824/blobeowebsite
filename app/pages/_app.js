290fee2422f224b475009fe822e949302adab576
AR <techrebel2723@gmail.com>
2021-07-27 23:17:38 +0000

    ,

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -3,10 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 function MyApp({ Component, pageProps }) {
   return ( 
   <>
-  <Component {...pageProps} /> <script
-  src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-  crossOrigin="anonymous"></script>
+  <Component {...pageProps} />
   </>
   )
 }
